20ee8da4c9a828f05f672a971486e24c4fad52c6
tom_glb <goodMorning_glb@atguigu.com>
2019-05-15 11:54:01 +0800

    创建装饰模式项目

---
+++ src/main/java/com/example/decorate_model/DecorateModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.decorate_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorateModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecorateModelApplication.class, args);
+    }
+
+}
